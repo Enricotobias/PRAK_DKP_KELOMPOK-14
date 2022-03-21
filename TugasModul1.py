38b6ad56bd39502dc00019bea49c194b860899ab
Enricotobias <102042758+Enricotobias@users.noreply.github.com>
2022-03-21 21:25:41 +0700

    Test Upload Tugas Modul 1

---
+++ TugasModul1.py
@@ -0,0 +1,35 @@
+print("\nIdentitas Kelompok : ")
+print("RAMADHIN PUTRA FADHLI, 21120121140101, Kel.14, Shift 1")
+print("SATRIA ADI PRATAMA, 21120121140154, Kel.14, Shift 1")
+print("MUHAMMAD HAIDAR KHOLIL ATHALLAH, 21120121120036, Kel.14, Shift 1")
+print("ENRICO TOBIAS, 21120121140113, Kel.14, Shift 1")
+print('')
+a = [['Mawar', 'Violet', 'Dendrum', 'Matahari'], ['500000', '600000', '650000', '725000']]
+print("Harga Villa di Puncak")
+print("=========================")
+print("Villa", a[0][0], "seharga", a[1][0], "rupiah per malam")
+print("Villa", a[0][1], "seharga", a[1][1], "rupiah per malam")
+print("Villa", a[0][2], "seharga", a[1][2], "rupiah per malam")
+print("Villa", a[0][3], "seharga", a[1][3], "rupiah per malam")
+
+# print ("Identifikasi Kelompok :")
+# print ('')
+# print ("RAMADHIN PUTRA FADHLI, 21120121140101, Kel.14, Shift 1")
+# print ("SATRIA ADI PRATAMA, 21120121140154, Kel.14, Shift 1")
+# print ("MUHAMMAD HAIDAR KHOLIL ATHALLAH, 21120121120036, Kel.14, Shift 1")
+# print ("ENRICO TOBIAS, 21120121140113, Kel.14, Shift 1")
+# print ('')
+# r = 14
+# lingkaran = 3/4
+# phi = 3.1413
+# luas =  lingkaran * phi * r * r        
+
+# print ("Dik : ")
+# print ("r =",r,"cm")
+# print ("Lingkaran =",lingkaran,"bagian")
+# print ("π =",phi)
+# print ("Dit = Luas?")
+# print ("Jawaban :")
+# print ("Luas = 3/4 x phi x r x r")
+# print ("Luas =",lingkaran,'x',phi,'x',r,'x',r)
+# print ("Luas =",round(luas),"cm\u00b2")
