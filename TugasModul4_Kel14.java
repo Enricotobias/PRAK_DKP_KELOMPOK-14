35f7f04b462dc54b2a21949ec3953df3159f9f25
satriaadipratama <102475517+satriaadipratama@users.noreply.github.com>
2022-03-27 23:00:35 +0700

    Update TugasModul4_Kel14.java

--- TugasModul4_Kel14.java
+++ TugasModul4_Kel14.java
@@ -29,4 +29,26 @@ class TugasModul4_Kel14 {
                 System.out.println("-----------------------------------------");
 
                 float hasil_IPA = 0, hasil_BIN = 0, hasil_IPS = 0, hasil_MAT =0, hasil_BI= 0;
- 
+ for (int i = 0; i < nilai.length; i++) {
+                        switch (i) {
+                                case 1:
+                                        printNilai("IPA");
+                                        System.out.print("Tugas : ");
+                                        int inputTugas_IPA = input.nextInt();
+                                        System.out.print("UTS   : ");
+                                        int inputUTS_IPA = input.nextInt();
+                                        System.out.print("UAS   : ");
+                                        int inputUAS_IPA = input.nextInt();
+                                        hasil_IPA = hitung(inputTugas_IPA, inputUTS_IPA, inputUAS_IPA);
+                                        break;
+
+                                case 2:
+                                        printNilai("IPS");
+                                        System.out.print("Tugas : ");
+                                        int inputTugas_IPS = input.nextInt();
+                                        System.out.print("UTS   : ");
+                                        int inputUTS_IPS = input.nextInt();
+                                        System.out.print("UAS   : ");
+                                        int inputUAS_IPS = input.nextInt();
+                                        hasil_IPS = hitung(inputTugas_IPS, inputUTS_IPS, inputUAS_IPS);
+                                        break;
