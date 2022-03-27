5b78f7a6f804030a84760c3a0e09c9f83aa92bf6
rputra102 <102019176+rputra102@users.noreply.github.com>
2022-03-27 22:58:38 +0700

    Update TugasModul4_Kel14.java

--- TugasModul4_Kel14.java
+++ TugasModul4_Kel14.java
@@ -29,3 +29,4 @@ class TugasModul4_Kel14 {
                 System.out.println("-----------------------------------------");
 
                 float hasil_IPA = 0, hasil_BIN = 0, hasil_IPS = 0, hasil_MAT =0, hasil_BI= 0;
+test 
